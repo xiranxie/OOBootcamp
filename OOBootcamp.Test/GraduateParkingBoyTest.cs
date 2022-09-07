b54afde971d47ff40aabca83f150007a1b0d4f0d
xiran xie <xiran.xie@thoughtworks.com>
2022-09-07 19:25:29 +0800

    test: AB#N/A - can park two cars

--- OOBootcamp.Test/GraduateParkingBoyTest.cs
+++ OOBootcamp.Test/GraduateParkingBoyTest.cs
@@ -38,8 +38,26 @@ public class GraduateBoyTest
         var parkingLot = _graduateParkingBoy.Park(vehicle);
         Assert.AreEqual("B",parkingLot.Name);
     }
-    
-    
-    
-    
+
+    [Test]
+    public void Should_Park_In_A_And_B_When_Parking_Given_Two_Vehicles()
+    {
+        var parkingLots = new List<ParkingLot>
+        {
+            new(2, 5, "A"),
+            new(2, 5, "B")
+        };
+        _parkingLots = new LinkedList<ParkingLot>(parkingLots);
+        _graduateParkingBoy = new GraduateParkingBoy(_parkingLots);
+        var vehicle1 = new Vehicle("123");
+        var vehicle2 = new Vehicle("456");
+        var parkingLot1 = _graduateParkingBoy.Park(vehicle1);
+        var parkingLot2 = _graduateParkingBoy.Park(vehicle2);
+        Assert.AreEqual("A",parkingLot1.Name);
+        Assert.AreEqual("B",parkingLot2.Name);
+    }
+
+
+
+
 }
--- OOBootcamp/GraduateParkingBoy.cs
+++ OOBootcamp/GraduateParkingBoy.cs
@@ -4,27 +4,25 @@ public class GraduateParkingBoy
 {
     // Write your logic here
     private readonly LinkedList<ParkingLot> _parkingLots;
+    private LinkedListNode<ParkingLot> _currentParkingLotNode;
 
     public GraduateParkingBoy(LinkedList<ParkingLot> parkingLots)
     {
         _parkingLots = parkingLots;
+        _currentParkingLotNode = parkingLots.First;
     }
 
     public ParkingLot Park(Vehicle vehicle)
     {
-        var parkingLotNode = _parkingLots.First;
+        var parkingLotNode = _currentParkingLotNode;
         if (parkingLotNode.Value.ParkVehicle(vehicle))
         {
+            _currentParkingLotNode = parkingLotNode.Next;
             return parkingLotNode.Value;
         }
-
-        var nextParkingLot = parkingLotNode.Next.Value;
-        if (nextParkingLot.ParkVehicle(vehicle))
-        {
-            return nextParkingLot;
-        }
-
-        throw new Exception("all full");
+        _currentParkingLotNode = parkingLotNode.Next;
+        Park(vehicle);
+        return new ParkingLot(0,0,"test");
 
     }
 }
