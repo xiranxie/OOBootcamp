74536014b59f22ba84351e7c97542197776e6fb0
xiran xie <xiran.xie@thoughtworks.com>
2022-09-07 19:47:21 +0800

    feat: AB#N/A - throw exception when all parking lots are full

--- OOBootcamp.Test/GraduateParkingBoyTest.cs
+++ OOBootcamp.Test/GraduateParkingBoyTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using NUnit.Framework;
 
@@ -24,20 +25,6 @@ public class GraduateBoyTest
         Assert.AreEqual("A",parkingLot.Name);
     }
     
-    [Test]
-    public void Should_Park_In_B_When_Parking_Given_A_Is_Full()
-    {
-        var parkingLots = new List<ParkingLot>
-        {
-            new(0, 5, "A"),
-            new(2, 5, "B")
-        };
-        _parkingLots = new LinkedList<ParkingLot>(parkingLots);
-        _graduateParkingBoy = new GraduateParkingBoy(_parkingLots);
-        var vehicle = new Vehicle("123");
-        var parkingLot = _graduateParkingBoy.Park(vehicle);
-        Assert.AreEqual("B",parkingLot.Name);
-    }
 
     [Test]
     public void Should_Park_In_A_And_B_When_Parking_Given_Two_Vehicles()
@@ -76,7 +63,28 @@ public class GraduateBoyTest
         var parkingLot3 = _graduateParkingBoy.Park(vehicle3);
         Assert.AreEqual("A",parkingLot1.Name);
         Assert.AreEqual("B",parkingLot2.Name);
+        Assert.AreEqual("A",parkingLot3.Name);
+    }
+    
+    [Test]
+    public void Should_Throw_Exception_When_Parking_3TH_Vehicle_Given_Three_Vehicles()
+    {
+        var parkingLots = new List<ParkingLot>
+        {
+            new(1, 5, "A"),
+            new(1, 5, "B")
+        };
+        _parkingLots = new LinkedList<ParkingLot>(parkingLots);
+        _graduateParkingBoy = new GraduateParkingBoy(_parkingLots);
+        var vehicle1 = new Vehicle("123");
+        var vehicle2 = new Vehicle("456");
+        var vehicle3 = new Vehicle("789");
+        var parkingLot1 = _graduateParkingBoy.Park(vehicle1);
+        var parkingLot2 = _graduateParkingBoy.Park(vehicle2);
         Assert.AreEqual("A",parkingLot1.Name);
+        Assert.AreEqual("B",parkingLot2.Name);
+        var exception = Assert.Throws<Exception>(() => _graduateParkingBoy.Park(vehicle3));
+        Assert.AreEqual("all full", exception.Message);
     }
 
 
--- OOBootcamp/GraduateParkingBoy.cs
+++ OOBootcamp/GraduateParkingBoy.cs
@@ -4,24 +4,32 @@ public class GraduateParkingBoy
 {
     private readonly LinkedList<ParkingLot> _parkingLots;
     private LinkedListNode<ParkingLot> _currentParkingLotNode;
+    private int _fulledParkingLots;
 
     public GraduateParkingBoy(LinkedList<ParkingLot> parkingLots)
     {
         _parkingLots = parkingLots;
         _currentParkingLotNode = parkingLots.First;
+        _fulledParkingLots = 0;
     }
 
     public ParkingLot Park(Vehicle vehicle)
     {
+        if (_parkingLots.Count == _fulledParkingLots)
+        {
+            throw new Exception("all full");
+        }
         var parkingLotNode = _currentParkingLotNode;
         if (parkingLotNode.Value.ParkVehicle(vehicle))
         {
             SetCurrentParkingLotNode(parkingLotNode);
             return parkingLotNode.Value;
         }
+
+        _fulledParkingLots++;
         SetCurrentParkingLotNode(parkingLotNode);
         Park(vehicle);
-        return new ParkingLot(0,0,"test");
+        return null;
     }
 
     private void SetCurrentParkingLotNode(LinkedListNode<ParkingLot> parkingLotNode)
